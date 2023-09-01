8c730937451f554ca3eec39264601a386630dd2d
ducpd <ducpd@deha-soft.com>
2023-09-01 22:45:59 +0700

    config route

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+include_once(__DIR__ . '/apis/v1.php');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
